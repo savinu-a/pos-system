b1c8670db6558495c0309df4d3d324379e9b348c
Savinu Anusara <savinu0002@gmail.com>
2024-03-20 19:00:50 +0530

    Add Serializable interface to Bill class and implement serialization and deserialization in Utils class

--- Bill/Bill.java
+++ Bill/Bill.java
@@ -3,11 +3,12 @@ package Bill;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.io.Serializable;
 
 
 
 
-public class Bill {
+public class Bill implements Serializable {
     private String cashier;
     private String date;
     private String branch;
@@ -15,6 +16,8 @@ public class Bill {
     private HashMap<String, ArrayList<Object>> items;
 
     public Bill(String cashier, String branch) {
+        this.cashier = cashier;
+        this.branch = branch;
         this.date = new Date().toString();
     }
     public void addItem(String key, ArrayList<Object> value, Integer quantity) {
@@ -34,21 +37,21 @@ public class Bill {
     public double totalDiscount(){
         double total = 0;
         for (ArrayList<Object> value : this.items.values()) {
-            total += (double)value.get(8);
-                
-            
+            total += (double)value.get(8);   
         }
         return Math.round(total * 100.0) / 100.0;
     }
-    
+
     public double totalAmount(){
         double total = 0;
         for (ArrayList<Object> value : this.items.values()) {
-            total += (double)value.get(7);
-                
-            
+            total += (double)value.get(7);   
         }
         return Math.round(total * 100.0) / 100.0;
+    }
+
+    public String getCashier() {
+        return cashier;
     }
 
 }
--- utils/Utils.java
+++ utils/Utils.java
@@ -1,11 +1,16 @@
 package utils;
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Hashtable;
+
+import Bill.Bill;
 
 public class Utils {
     final static String ITEM_LIST_PATH = "db/itemList.txt";
@@ -39,4 +44,25 @@ public class Utils {
         return items;
 
     }
+    public static void serialize(Bill bill, String filename) {
+        try (FileOutputStream fileOut = new FileOutputStream(filename);
+             ObjectOutputStream objectOut = new ObjectOutputStream(fileOut)) {
+            objectOut.writeObject(bill);
+            System.out.println("The object has been serialized and saved in " + filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static Bill deserialize(String filename) {
+        Bill bill = null;
+        try (FileInputStream fileIn = new FileInputStream(filename);
+             ObjectInputStream objectIn = new ObjectInputStream(fileIn)) {
+            bill = (Bill) objectIn.readObject();
+            System.out.println("The object has been deserialized from " + filename);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return bill;
+    }
+
 }
