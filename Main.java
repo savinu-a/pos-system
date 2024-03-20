bbeb11e1e2d7b067782d817933a2a934b7947234
Tumasha Deshan <tumashadeshan@gmail.com>
2024-03-20 17:26:35 +0530

    Added Screens

---
+++ Main.java
@@ -0,0 +1,8 @@
+import Screens.MainScreen;
+
+class Main {
+    public static void main(String[] args) {
+        MainScreen mainScreen = new MainScreen();
+        mainScreen.mainScreen();
+    }
+}
